3bd6da54bca134d4df0b12402cec28f8ed945278
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2019-10-08 12:48:13 -0400

    Contexto creado.

---
+++ RegistroAsistencia/DAL/Contexto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RegistroAsistencia.Entidades;
+
+namespace RegistroAsistencia.DAL
+{
+    public class Contexto: DbContext
+    {
+       
+        public DbSet<Asistencia> Asistencia { get; set; } 
+        public DbSet<Estudiante> Estudiante { get; set; } 
+        public DbSet<Asignatura> Asignatura { get; set; }
+
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+
+}
